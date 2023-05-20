b9d3bc5a05c0d992729f5e70af3980b1f4694fc0
Kenneth Aidoo <kennethnumba7@gmail.com>
2023-05-20 18:40:26 -0400

    Initial system conceptualization

--- car.py
+++ car.py
@@ -1,10 +1,13 @@
+#abc is a module and ABC is a helper class that allows us to define abstract classes 
+#abstractmethod is a decorator used to declare abstract methods within a class.
 from abc import ABC, abstractmethod
 
 
+#Car inherits ABC so it can be an abs class
 class Car(ABC):
     def __init__(self, last_service_date):
         self.last_service_date = last_service_date
 
     @abstractmethod
-    def needs_service(self):
+    def needs_service(self) -> bool:
         pass
--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,13 +1,14 @@
 from abc import ABC
-
 from car import Car
 
 
 class CapuletEngine(Car, ABC):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+        super().__init__(last_service_date) #calls value from parent class
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def engine_should_be_serviced(self) -> bool:
+        #if the value is more than 30000 returns true
+        #return true if the current mileage < last mileage
         return self.current_mileage - self.last_service_mileage > 30000
