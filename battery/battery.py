b094a6b493fd840c3aec6424a3d970ce06b22cd0
Kenneth Aidoo <kennethnumba7@gmail.com>
2023-06-11 09:04:22 -0400

    Added some comments

--- battery/battery.py
+++ battery/battery.py
@@ -1,5 +1,7 @@
 from abc import ABC, abstractmethod
 
+#modifying the behvaiour of a part for all cars 
+#requires just modifying the interface
 class Battery(ABC):
     @abstractmethod
     def needs_service(self) -> bool:
