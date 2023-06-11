5905a5d1b3ed71e87440c9f22c0c9b2776912cca
Kenneth Aidoo <kennethnumba7@gmail.com>
2023-06-11 09:02:26 -0400

    Made some slight formatting adjustments

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -3,9 +3,9 @@ from engine import Engine
 
 
 class CapuletEngine(Engine, ABC):
-    def __init__(self, current_mileage: int, last_service_mileage: int):
-        self._current_mileage: int = current_mileage          #private
+    def __init__(self, last_service_mileage: int, current_mileage: int):
         self._last_service_mileage: int = last_service_mileage   #private
+        self._current_mileage: int = current_mileage             #private
 
     def needs_service(self) -> bool:  
         super().needs_service()
--- engine/willoughby_engine.py
+++ engine/willoughby_engine.py
@@ -2,9 +2,9 @@ from abc import ABC
 from engine import Engine
 
 class WilloughbyEngine(Engine, ABC):
-    def __init__(self, current_mileage, last_service_mileage):
-        self._current_mileage: int = current_mileage             #private
+    def __init__(self, last_service_mileage: int, current_mileage: int):
         self._last_service_mileage: int = last_service_mileage   #private
+        self._current_mileage: int = current_mileage             #private
 
     def needs_service(self) -> bool:
         super().needs_service() #call from parent engine class
