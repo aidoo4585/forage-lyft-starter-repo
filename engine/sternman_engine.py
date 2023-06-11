d47be422ec079f3631ededbed17d4527a9418fee
Kenneth Aidoo <kennethnumba7@gmail.com>
2023-06-11 08:40:59 -0400

    Refactored sternman child class

--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,15 +1,16 @@
 from abc import ABC
 
-from car import Car
+from engine import Engine
 
 
-class SternmanEngine(Car, ABC):
-    def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
-        self.warning_light_is_on = warning_light_is_on
+class SternmanEngine(Engine, ABC):
+    def __init__(self, warning_light_is_on):
+        self._warning_light_on = warning_light_is_on    #private 
 
-    def engine_should_be_serviced(self):
-        if self.warning_light_is_on:
+    def needs_service(self):
+        super().needs_service()
+        
+        if self._warning_light_is_on:
             return True
         else:
             return False
