fa0a08d53c94bab780e1091444a2680326275296
Kenneth Aidoo <kennethnumba7@gmail.com>
2023-06-11 09:11:23 -0400

    Added some minor chages to car interface

--- car.py
+++ car.py
@@ -1,11 +1,6 @@
-#abc is a module and ABC is a helper class that allows us to define abstract classes 
-#abstractmethod is a decorator used to declare abstract methods within a class.
 from abc import ABC, abstractmethod
 from engine import Engine
 from battery import Battery
-
-
-#Car inherits ABC so it can be an abc class
 class Car(ABC):
     @abstractmethod
     def __init__(self, engine:Engine, battery: Battery ) -> None:
